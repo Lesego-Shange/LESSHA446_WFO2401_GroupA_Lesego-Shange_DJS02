f602f0904cf8ad3dd0a8bffee87327e267effe3a
Lesego Shange <makgeledisal@gmail.com>
2024-05-12 19:57:53 +0200

    Convert inputs

--- scripts.js
+++ scripts.js
@@ -6,4 +6,8 @@ form.addEventListener("submit", (event) => {
   const entries = new FormData(event.target);
   const { dividend, divider } = Object.fromEntries(entries);
   result.innerText = dividend / divider;
+
+  // Convert inputs to numbers
+  const dividendNum = parseFloat(dividend);
+  const dividerNum = parseFloat(divider);
 });
